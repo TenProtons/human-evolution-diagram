326d9d5e47904b6545a6aa383ed65191e52d255d
M.Bondarenko <m.bondarenko@upswot.com>
2025-01-20 16:50:04 +0100

    in process

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/human-evolution-diagram/',  // <-- important for GH Pages
   plugins: [react()],
 })
