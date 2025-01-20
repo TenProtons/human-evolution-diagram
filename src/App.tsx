42467a6698bb76e34e33edb85b4e7754255902b3
M.Bondarenko <m.bondarenko@upswot.com>
2025-01-20 21:03:23 +0100

    in process

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import Diagram from './components/Diagram/Diagram'
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import Diagram from "./components/Diagram/Diagram";
+import "./App.scss";
 
 function App() {
   const { t, i18n } = useTranslation();
@@ -9,30 +10,33 @@ function App() {
   // Toggle theme
   React.useEffect(() => {
     if (isDarkMode) {
-      document.body.classList.add('theme-dark');
+      document.body.classList.add("theme-dark");
     } else {
-      document.body.classList.remove('theme-dark');
+      document.body.classList.remove("theme-dark");
     }
   }, [isDarkMode]);
 
   // Toggle language
   const toggleLanguage = () => {
-    i18n.changeLanguage(i18n.language === 'en' ? 'uk' : 'en');
+    i18n.changeLanguage(i18n.language === "en" ? "uk" : "en");
   };
 
   return (
     <div className="app">
-      <button onClick={() => setIsDarkMode(!isDarkMode)}>
-        {isDarkMode ? 'Light Theme' : 'Dark Theme'}
-      </button>
-      <button onClick={toggleLanguage}>
-        {i18n.language === 'en' ? 'Switch to Ukrainian' : 'Switch to English'}
-      </button>
+      <section className="app__controls">
+        <button className="regular-button" onClick={toggleLanguage}>
+          {i18n.language === "en" ? "Ukrainian" : "English"}
+        </button>
+        <button className="regular-button" onClick={() => setIsDarkMode(!isDarkMode)}>
+          {isDarkMode ? "Light Theme" : "Dark Theme"}
+        </button>
+      </section>
 
-      <h1>{t('title')}</h1>
-      <p>{t('subtitle')}</p>
+      <section className="app__header">
+        <h1>{t("title")}</h1>
+        <p>{t("subtitle")}</p>
+      </section>
 
-      {/* Render the diagram */}
       <Diagram />
     </div>
   );
